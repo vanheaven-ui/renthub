d4e1367e2db3d899a1141f16ac5c38b2ba42559c
Mworekwa Ezekiel <vanheaven6@gmail.com>
2025-08-24 12:28:59 +0300

    feat:Add token-based authentication with JWT - Implement admin authorization middleware

---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "my_fallback_secret";
+
+export interface AuthRequest extends Request {
+  user?: {
+    userId: string;
+    email: string;
+    role?: 'ADMIN' | 'OWNER' | 'RENTER';
+  };
+}
+
+
+export const authenticateToken = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.token;
+
+  console.log("TOKEN IS HERE: ", token)
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Authentication token is required" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as {
+      userId: string;
+      email: string;
+    };
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    return res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
